8ede85113c277b82e407f72e772fecc09ca26496
Iñaki Garay <igarai@gmail.com>
2018-11-21 14:13:04 -0300

    rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 extern crate notify;
 
-use notify::{RecommendedWatcher, Watcher, RecursiveMode};
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
 fn watch() -> notify::Result<()> {
-  let (tx, rx) = channel();
-  let mut watcher: RecommendedWatcher = try!(Watcher::new(tx, Duration::from_secs(2)));
-  try!(watcher.watch("/Users/aki/Downloads/", RecursiveMode::Recursive));
-  loop {
-    match rx.recv() {
-      Ok(event) => println!("{:?}", event),
-      Err(e) => println!("watch error: {:?}", e),
+    let (tx, rx) = channel();
+    let mut watcher: RecommendedWatcher = try!(Watcher::new(tx, Duration::from_secs(2)));
+    try!(watcher.watch("/Users/aki/Downloads/", RecursiveMode::Recursive));
+    loop {
+        match rx.recv() {
+            Ok(event) => println!("{:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
     }
-  }
-}
-  
-fn main() {
-  if let Err(e) = watch() {
-    println!("error: {:?}", e)
-  }
 }
 
+fn main() {
+    if let Err(e) = watch() {
+        println!("error: {:?}", e)
+    }
+}
